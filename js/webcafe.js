9199e5fc0470d721799e3f4c3128835c53ff8ad9
seulkicho <wjsrkfRhfl@gmail.com>
2017-08-14 18:06:47 +0900

    마크다운 링크 수정

--- js/webcafe.js
+++ js/webcafe.js
@@ -2,4 +2,7 @@ $(function() {
     $('.btn-menubar').on('click', function() {
         $(this).parent().siblings().toggleClass('main-menu-act');
     });
+    $('.btn-menubar').on('click', function() {
+        $(this).parent().siblings().toggleClass('main-menu-act');
+    });
 });
